dc23b6d9c91f241d0a6aed967ecb992b62cdae84
Matej Kramny <git@matej.me>
2016-10-04 15:15:04 +0100

    Update lamp.sh

--- conf/lamp.sh
+++ conf/lamp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tail -f /var/log/apache2/*.log /var/log/apache2/website.err
+tail -f /var/log/apache2/*.log /var/log/apache2/website.err &
 source /etc/apache2/envvars && exec /usr/sbin/apache2 -D FOREGROUND
